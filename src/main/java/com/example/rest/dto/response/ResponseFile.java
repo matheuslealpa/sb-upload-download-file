da0a0a43214255dd1b6527d13d15a86ee0b3505b
Matheus Leal <matheuslealpa@gmail.com>
2022-12-24 15:02:05 -0300

    feat: springboot upload/download files to/from database example

---
+++ src/main/java/com/example/rest/dto/response/ResponseFile.java
@@ -0,0 +1,15 @@
+package com.example.rest.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseFile {
+    private String name;
+    private String url;
+    private String type;
+    private long size;
+}
