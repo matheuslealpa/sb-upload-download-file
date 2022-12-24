713bcdc0e88c5ba3b4b9a9844d690a46c0477620
Matheus Leal <matheuslealpa@gmail.com>
2022-12-24 14:41:52 -0300

    refactor: refactor name main method

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbUploadDownloadFileApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbUploadDownloadFileApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
