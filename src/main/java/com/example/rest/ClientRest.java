a3992ce1c7fe3931b6e17db027bbaba755d560a8
Matheus Leal <matheuslealpa@gmail.com>
2022-12-25 22:04:35 -0300

    feat: method post de inserçao do cliente

--- src/main/java/com/example/rest/ClientRest.java
+++ src/main/java/com/example/rest/ClientRest.java
@@ -1,6 +1,7 @@
 package com.example.rest;
 
 import com.example.domain.Client;
+import com.example.domain.dto.ClientDTO;
 import com.example.service.ClientService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/clients")
 public class ClientRest {
     private ClientService clientService;
+
+    @PostMapping
+    public ResponseEntity<Client> insert(ClientDTO clientDTO) throws IOException {
+        return ResponseEntity.ok(clientService.insert(clientDTO));
+    }
 }
